635d50e26d293aeb29fba898dfc6a9f0a4612bd2
Norman Mwagi <normanmwagi23@gmail.com>
2025-06-10 12:40:18 +0300

    webpack merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -22,20 +16,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,                // 👈 Add this rule
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
-        },
+      },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
       {
-  test: /\.(png|svg|jpg|jpeg|gif)$/i,
-  type: "asset/resource",
-}
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
     ],
   },
 };
